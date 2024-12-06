83c7c38e19884e5f4e09a77f73df39a7f7ea5b05
pkziinn10 <pedrinhokauan824@gmail.com>
2024-12-06 19:17:53 -0300

    Exercise csharp

---
+++ Exercício 1/classes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Exercise
+{
+    class Person
+    {
+        public string? name;
+        public int age;
+    }
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person();
+
+            Console.WriteLine("Digite o nome da primeira Pessoa: ");
+            p1.name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(p1.name) || !p1.name.All(Char.IsLetter))
+            {
+                Console.WriteLine("Nome Inválido");
+                return;
+            }
+
+            Console.WriteLine("Digite a idade:");
+            p1.age = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Digite o nome da segunda pessoa:");
+            p2.name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(p2.name) || !p2.name.All(Char.IsLetter))
+            {
+                Console.WriteLine("Nome Inválido");
+                return;
+            }
+
+            Console.WriteLine("Digite a idade: ");
+            p2.age = int.Parse(Console.ReadLine() ?? "0");
+
+            if (p1.age > p2.age)
+            {
+                Console.WriteLine($"{p1.name} É mais velho que {p2.name} ");
+            }
+            else
+            {
+                Console.WriteLine($"{p2.name} é mais velho que {p1.name} ");
+            }
+        }
+    }
+}
---
+++ Exercício 2/classes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace exercise2
+{
+    class Employee
+    {
+        public string? name;
+        public double salary;
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee();
+
+            Console.WriteLine("Digite o nome do funcionário: ");
+            emp1.name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(emp1.name) || !emp1.name.All(Char.IsLetter))
+            {
+                Console.WriteLine("Nome do funcionário Inválido: ");
+                return;
+            }
+
+            Console.WriteLine("Digite o sálario do funcionário: ");
+            if (!double.TryParse(Console.ReadLine(), out emp1.salary))
+            {
+                Console.WriteLine("Valor de Sálario Inválido: ");
+                return;
+            }
+
+            Console.WriteLine("Digite o nome do funcionário: ");
+            emp2.name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(emp2.name) || !emp1.name.All(Char.IsLetter))
+            {
+                Console.WriteLine("Nome do funcionário Inválido: ");
+                return;
+            }
+
+            Console.WriteLine("Digite o sálario do funcionário: ");
+
+            if (!double.TryParse(Console.ReadLine(), out emp2.salary))
+            {
+                Console.WriteLine("Valor de Sálario Inválido:");
+                return;
+            }
+
+            double media = (emp1.salary + emp2.salary) / 2.0;
+
+            Console.WriteLine($"A média salarial dos funcionários é: {media.ToString("C", CultureInfo.CurrentCulture)}");
+        }
+    }
+}
